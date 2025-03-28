8f5d42ebd9c522e94b4bd986425e213b5cabd458
CelsoV25 <celsoprincipal@outlook.com>
2025-03-27 21:17:55 -0300

    Update Para CartasSuperTrunfo.c
    Ajustei o erro da primeira versão que não era possível inserir duas cartas com o formato de estrutura e adicionei as informações do novo desafio pedido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,48 +1,62 @@
 #include <stdio.h>
 
-int main(){
-    int populacao, pontos, carta, codigo;
-    float area, PIB;
-    char  estad[2];
+typedef struct{
+    int populacao, pontos, codigo_carta, codigo;
+    float area, PIB, densidade, PIBpercapita;
+    char  estad[3];
     char cidade[30];
+} Carta;
 
-    printf("Digite seu código de carta: \n");
-    scanf("%d", &carta);
+int main() {
+    Carta cartas[2];
 
-    printf("Digite a sigla do seu estado: \n");
-    scanf("%2s", &estad);
+    for(int i = 0; i < 2; i++) {
+    
+        printf("\n -- Coletando dados da carta %d --\n", i+1);
 
-    printf("Digite o seu código: \n");
-    scanf("%d", &codigo); 
+        printf("Digite seu código de carta: \n");
+        scanf("%d", &cartas[i].codigo_carta);
 
-    printf("Digite a sua cidade: \n");
-    scanf("%29s", &cidade);
+        printf("Digite a sigla do seu estado: \n");
+        scanf("%3s", &cartas[i].estad);
 
-    printf("População: \n");
-    scanf("%d", &populacao);
+        printf("Digite o seu código: \n");
+        scanf("%d", &cartas[i].codigo); 
 
-    printf("Digite a área em km²: \n");
-    scanf("%f", &area);
+        printf("Digite a sua cidade: \n");
+        scanf("%29s", &cartas[i].cidade);
 
-    printf("Qual o PIB da sua cidade em reais? \n");
-    scanf("%f", &PIB);
+        printf("População: \n");
+        scanf("%d", &cartas[i].populacao);
 
-    printf("Qual a quantidade de pontos turísticos? \n");
-    scanf("%d", &pontos);
+        printf("Digite a área em km²: \n");
+        scanf("%f", &cartas[i].area);
 
+        printf("Qual o PIB da sua cidade em reais? \n");
+        scanf("%f", &cartas[i].PIB);
 
+        printf("Qual a quantidade de pontos turísticos? \n");
+        scanf("%d", &cartas[i].pontos);
 
+        cartas[i].densidade = cartas[i].populacao / cartas[i].area;
+        cartas[i].PIBpercapita = cartas[i].PIB / (float)cartas[i].populacao;
+
+    }
+
+    for(int i = 0; i < 2; i++) {
     printf("\n -- Dados inseridos da carta --\n");
-    printf("Carta 1: %d\n", carta);
-    printf("Estado: %s\n", estad);
-    printf("Código: %d\n", codigo);
-    printf("Nome da cidade: %s\n", cidade);
-    printf("População: %d de habitantes\n", populacao);
-    printf("Área: %.2f km²\n", area);
-    printf("PIB: %.2f de reais\n", PIB);
-    printf("Número de pontos turísticos: %d\n", pontos);
+    printf("Código da carta: %d\n", cartas[i].codigo_carta);
+    printf("Estado: %s\n", cartas[i].estad);
+    printf("Código: %d\n", cartas[i].codigo);
+    printf("Nome da cidade: %s\n", cartas[i].cidade);
+    printf("População: %d de habitantes\n", cartas[i].populacao);
+    printf("Área: %.2f km²\n", cartas[i].area);
+    printf("PIB: %.2f de reais\n", cartas[i].PIB);
+    printf("Número de pontos turísticos: %d\n", cartas[i].pontos);
+    printf("A densidade populacional da cidade é: %.2f hab/km²\n", cartas[i].densidade);
+    printf("O PIB per capita da cidade em questão é:R$ %.2f\n", cartas[i].PIBpercapita);
 
-
+    }
     return 0;
 
 }
